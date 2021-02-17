38135e692656df568919ab1645440405050d2bf9
Mohamed Fawzy <m.fawzy.a@icloud.com>
2021-02-17 21:51:43 +0200

    Initial Commit

---
+++ Rio Restaurant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Rio Restaurant
+//
+//  Created by Mohamed Fawzy on 17/02/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
